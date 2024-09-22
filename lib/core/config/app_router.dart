f1d536092fbceea996f078d05f51fd4465d2c1e7
Pedro Martin <pmargom@gmail.com>
2024-09-22 15:43:31 +0100

    Configure app router

---
+++ lib/core/config/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:go_router/go_router.dart';
+import 'package:imgur/core/config/routes.dart';
+import 'package:imgur/presentation/screens/details_screen.dart';
+import 'package:imgur/presentation/screens/home_screen.dart';
+
+// GoRouter configuration
+final appRouter = GoRouter(
+  initialLocation: Routes.home,
+  routes: [
+    GoRoute(
+      path: Routes.home,
+      builder: (_, __) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: Routes.details,
+      builder: (_, state) => DetailsScreen(goRouterState: state),
+    ),
+  ],
+);
