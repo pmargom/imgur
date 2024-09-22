742f05aa9f5f450fb27162722bf8113980fd9ae5
Pedro Martin <pmargom@gmail.com>
2024-09-22 15:48:58 +0100

    Create reusable widget to load images from network

---
+++ lib/presentation/widgets/custom_network_image.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomNetworkImage extends StatelessWidget {
+  const CustomNetworkImage({
+    super.key,
+    required this.firstImageUrl,
+  });
+
+  final String firstImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      firstImageUrl,
+      fit: BoxFit.cover,
+      loadingBuilder: _loadingBuilder,
+      errorBuilder: _errorBuilder,
+    );
+  }
+
+  Widget _loadingBuilder(_, Widget child, ImageChunkEvent? loadingProgress) {
+    if (loadingProgress == null) {
+      return child;
+    } else {
+      return Center(
+        child: CircularProgressIndicator(
+          value:
+              loadingProgress.expectedTotalBytes != null ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1) : null,
+        ),
+      );
+    }
+  }
+
+  Widget _errorBuilder(_, __, ___) => const Center(
+        child: Text('Error during image loading'),
+      );
+}
